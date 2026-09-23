4db1679bdababe477ff5ce2f5ff049ef1d97334a
Jaehwan Lee <100256315+jaehwan-2ee@users.noreply.github.com>
2026-09-23 07:47:21 +0000

    pitex v1.7.0

--- Casks/pitex.rb
+++ Casks/pitex.rb
@@ -1,6 +1,6 @@
 cask "pitex" do
-  version "1.6.5"
-  sha256 "4d00e6f243fe093c74414650f9a8bf1c6adc1ead52fc9d4e89c4832fc9713c7f"
+  version "1.7.0"
+  sha256 "2020271767896d67cc3f0757ec0f51b4895e31ef9378d8cdce19cda4d098004e"
 
   url "https://github.com/jaehwan-2ee/pitex/releases/download/v#{version}/Pitex-v#{version}-macos-arm64.dmg"
   name "Pitex"
